eeffc4a4cb9ff18c09e71d6338440978d9fe4bed
Aaron Sharma <79173907+AaronS2305@users.noreply.github.com>
2021-11-24 22:38:13 +0000

    Added loops

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Number Guesseing Game version 1
+#Number Guesseing Game version 2
 #25/11/2021
 #Aaron Sharma
 
@@ -9,15 +9,25 @@ import random
 #Target variable to store a random target number for the user to guess
 target = random.randint(1, 100)
 #guess variable to store user's guessed number
-guess = int(input("Please guess a number between 1-100"))
 
-if guess == target:      #If user guesses target number
-  print("Congratulations, you guessed correctly")
-elif guess < target:  #if user guess is below target number 
-  print("Your guess was too low")
-elif guess > target:   #if user guess is higher than target number
-  print("Your guess was too high")
-elif guess < 1:    #if user guess is higher than range
-  print("Invalid entry, please try again")
-elif guess > 100:     #if user guess is lower than range
-  print("Invalid entry, please try again")
+
+run_loop = True
+
+while run_loop:
+  guess = int(input("Please guess a number between 1-100"))
+
+  while not(guess >=1 and guess <= 100):
+    guess = int(input("That is not a valid entry, Please enter a number between 1-100"))
+
+
+
+  if guess < target:
+   print("Your guess was too low")
+  elif guess > target:
+    print("Your guess was too high")
+  elif guess == target:
+    run_loop = False
+
+
+
+print("Congratulations, you guessed correctly")
