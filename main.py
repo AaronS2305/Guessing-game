3e3bee604dfb9c23fd2475b16c0b3a69d539571d
AaronS2305 <79173907+AaronS2305@users.noreply.github.com>
2021-11-25 14:17:58 +1300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ max_length_list = 3
 name_country = ""
 
 
-
+#Collecting data
 while len(name_country) < max_length_list:
   name_country = str(input("Please enter a country name")).upper()
   country_list.append(name_country)
@@ -30,15 +30,17 @@ print(country_list,"is the country and ", num_infected, "is the amount infected"
 
 
 
-
+#Function to print details
 def print_details(self):
   details = "" + self.name_country + "currently has " + str(self.infected_percent) + "percent infected, and "
   if self.is_in_lockdown == True:
     details += "is in lockdown"
 
+#Function to ask for help (work in progress)
 def ask_for_assist(name_country):
   ask_help = input("Which country would you like to assist?, Choose from the following")
   for name_country in country_list:
     ask_help += "\n" + name_country.country_list
     while True:
       ask_for_assist
+
